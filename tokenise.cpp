812e3ef2b6e1410650af0d672142a4be03107f0e
brianvansteen <bvansteen@gmail.com>
2022-06-11 11:13:05 -0400

    updates

--- tokenise.cpp
+++ tokenise.cpp
@@ -56,9 +56,17 @@ int main()
                 continue;
             }
 
-            double price = std::stod(tokens[3]);
-            double amount = std::stod(tokens[4]);
-            std::cout<<price<<": "<<amount<<std::endl;
+            try {
+                double price = std::stod(tokens[3]);
+                double amount = std::stod(tokens[4]);
+                std::cout<<price<<": "<<amount<<std::endl;
+                
+            } catch (const std::exception& e) {
+                std::cout<<"Bad float: "<<tokens[3]<<std::endl;
+                std::cout<<"Bad float: "<<tokens[4]<<std::endl;
+                continue;
+            }
+
 
             for (std::string& t : tokens)
             {
